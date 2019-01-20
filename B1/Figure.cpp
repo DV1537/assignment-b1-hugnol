07ab178adea4ff171f3ade8e1f544da9d3691feb
hugnol <44838038+hugnol@users.noreply.github.com>
2019-01-20 14:23:22 +0100

    Add files via upload

---
+++ B1/Figure.cpp
@@ -0,0 +1,170 @@
+//
+// Created by hugon on 16/01/2019.
+//
+
+#include "Figure.h"
+
+Figure::Figure() : currentShapeAmount(0), maxShapeAmount(5) {
+    groupedShapes = new Shape *[maxShapeAmount];
+}
+
+Figure::~Figure() {
+    if (groupedShapes) {
+        delete[] groupedShapes;
+        groupedShapes = nullptr;
+    }
+}
+
+void Figure::addShape(Shape *s) {  //Shape adder
+    if (currentShapeAmount == maxShapeAmount) {
+        transfer = new Shape *[maxShapeAmount];
+        for (int i = 0; i < maxShapeAmount; i++) {
+            transfer[i] = groupedShapes[i];
+        }
+        delete[] groupedShapes;
+        maxShapeAmount = maxShapeAmount * 2;
+        groupedShapes = new Shape *[maxShapeAmount];
+        for (int i = 0; i < currentShapeAmount; i++) {
+            groupedShapes[i] = transfer[i];
+        }
+        delete[] transfer;
+        transfer = nullptr;
+        groupedShapes[currentShapeAmount] = s;
+        currentShapeAmount++;
+        std::cout << groupedShapes[0]->getType() << std::endl;
+    } else {
+        groupedShapes[currentShapeAmount] = s;
+        std::cout << groupedShapes[currentShapeAmount]->getType() << std::endl;
+        currentShapeAmount++;
+    }
+}
+
+Line Figure::getBoundingBox() {
+    Point topLeft(0, 0), bottomRight(0, 0);
+
+    for (int i = 0; i < currentShapeAmount; i++) {
+        if (groupedShapes[i]->getType() == sPolygon) {
+            auto *currentShape = dynamic_cast<Polygon *>(groupedShapes[i]);
+            if(i == 0) {
+                bottomRight.X = currentShape->points[i].X;
+                bottomRight.Y = currentShape->points[i].Y;
+                topLeft.X = bottomRight.X;
+                topLeft.Y = bottomRight.Y;
+            }
+            for(int i = 0; i < currentShape->getPointsAmount(); i++) {
+                if(currentShape->points[i].X < topLeft.X) {
+                    topLeft.X = currentShape->points[i].X;
+                }
+                if(currentShape->points[i].Y > topLeft.Y) {
+                    topLeft.Y = currentShape->points[i].Y;
+                }
+                if(currentShape->points[i].X > bottomRight.X) {
+                    bottomRight.X = currentShape->points[i].X;
+                }
+                if(currentShape->points[i].Y < bottomRight.Y) {
+                    bottomRight.Y = currentShape->points[i].Y;
+                }
+            }
+        } else if (groupedShapes[i]->getType() == sTriangle) {
+            auto *currentShape = dynamic_cast<Triangle *>(groupedShapes[i]);
+            if(i == 0) {
+                bottomRight.X = currentShape->a.X;
+                bottomRight.Y = currentShape->a.Y;
+                topLeft.X = bottomRight.X;
+                topLeft.Y = bottomRight.Y;
+            }
+            if (currentShape->a.X < topLeft.X) {
+                topLeft.X = currentShape->a.X;
+            }
+            if (currentShape->a.Y > topLeft.Y) {
+                topLeft.Y = currentShape->a.Y;
+            }
+            if (currentShape->b.X < topLeft.X) {
+                topLeft.X = currentShape->b.X;
+            }
+            if (currentShape->b.Y > topLeft.Y) {
+                topLeft.Y = currentShape->b.Y;
+            }
+            if (currentShape->c.X < topLeft.X) {
+                topLeft.X = currentShape->c.X;
+            }
+            if (currentShape->c.Y > topLeft.Y) {
+                topLeft.Y = currentShape->c.Y;
+            }
+
+            if (currentShape->a.X > bottomRight.X) {
+                bottomRight.X = currentShape->a.X;
+            }
+            if (currentShape->a.Y < bottomRight.Y) {
+                bottomRight.Y = currentShape->a.Y;
+            }
+            if (currentShape->b.X > bottomRight.X) {
+                bottomRight.X = currentShape->b.X;
+            }
+            if (currentShape->b.Y < bottomRight.Y) {
+                bottomRight.Y = currentShape->b.Y;
+            }
+            if (currentShape->c.X > bottomRight.X) {
+                bottomRight.X = currentShape->c.X;
+            }
+            if (currentShape->c.Y < bottomRight.Y) {
+                bottomRight.Y = currentShape->c.Y;
+            }
+        } else if (groupedShapes[i]->getType() == sLine) {
+            auto *currentShape = dynamic_cast<Line *>(groupedShapes[i]);
+            if(i == 0) {
+                bottomRight.X = currentShape->a.X;
+                bottomRight.Y = currentShape->a.Y;
+                topLeft.X = bottomRight.X;
+                topLeft.Y = bottomRight.Y;
+            }
+            if (currentShape->a.X < topLeft.X) {
+                topLeft.X = currentShape->a.X;
+            }
+            if (currentShape->a.Y > topLeft.Y) {
+                topLeft.Y = currentShape->a.Y;
+            }
+            if (currentShape->b.X < topLeft.X) {
+                topLeft.X = currentShape->b.X;
+            }
+            if (currentShape->b.Y > topLeft.Y) {
+                topLeft.Y = currentShape->b.Y;
+            }
+            if (currentShape->a.X > bottomRight.X) {
+                bottomRight.X = currentShape->a.X;
+            }
+            if (currentShape->a.Y < bottomRight.Y) {
+                bottomRight.Y = currentShape->a.Y;
+            }
+            if (currentShape->b.X > bottomRight.X) {
+                bottomRight.X = currentShape->b.X;
+            }
+            if (currentShape->b.Y < bottomRight.Y) {
+                bottomRight.Y = currentShape->b.Y;
+            }
+
+        } else if (groupedShapes[i]->getType() == sPoint) {
+            auto *currentShape = dynamic_cast<Point *>(groupedShapes[i]);
+            if(i == 0) {
+                bottomRight.X = currentShape->X;
+                bottomRight.Y = currentShape->Y;
+                topLeft.X = bottomRight.X;
+                topLeft.Y = bottomRight.Y;
+            }
+            if (currentShape->X < topLeft.X) {
+                topLeft.X = currentShape->X;
+            }
+            if (currentShape->Y > topLeft.Y) {
+                topLeft.Y = currentShape->Y;
+            }
+            if (currentShape->X > bottomRight.X) {
+                bottomRight.X = currentShape->X;
+            }
+            if (currentShape->Y < bottomRight.Y) {
+                bottomRight.Y = currentShape->Y;
+            }
+        }
+    }
+    Line boundingLine(topLeft, bottomRight);
+    return boundingLine;
+}
---
+++ B1/Line.cpp
@@ -0,0 +1,39 @@
+//
+// Created by hugon on 03/12/2018.
+//
+
+#include "Line.h"
+
+CenterPoint Line::position() {
+    double centerX = (a.X + b.X) / 2;
+    double centerY = (a.Y + b.Y) / 2;
+
+    CenterPoint lneCenter(centerX, centerY);
+    return lneCenter;
+}
+
+double Line::distance(Shape *s) {
+    CenterPoint sCenter = s->position();    //Center of input Shape
+    CenterPoint lneCenter = position();     //Center of current Line
+
+    double distance = sqrt(pow(lneCenter.X - sCenter.X, 2) + pow(lneCenter.Y - sCenter.Y, 2));
+    return distance;
+}
+
+Line& Line::operator=(const Line &a) {
+    this->a.X = a.a.X;
+    this->a.Y = a.a.Y;
+    this->b.X = a.b.X;
+    this->b.Y = a.b.Y;
+
+    return *this;
+}
+
+std::ostream &Line::operator<<(std::ostream &os) {
+    os << type << std::endl;
+    os << "Vertices:" << std::endl;
+    os << " (" << a.X << ", " << a.Y << ") " << std::endl;
+    os << " (" << b.X << ", " << b.Y << ") " << std::endl;
+
+    return os;
+}
